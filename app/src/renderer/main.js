5a6f6cff74aa2152b50a30d9bc7d4fe945de1a11
Meet Godhani <meet_godhani1@yahoo.com>
2017-02-04 10:26:08 -0500

    Added updated structure.

---
+++ app/src/renderer/main.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
+import Electron from 'vue-electron';
+import Router from 'vue-router';
+
+import App from './App';
+import routes from './routes';
+
+Vue.use(Electron);
+Vue.use(ElementUI);
+Vue.use(Router);
+Vue.config.debug = true;
+
+const router = new Router({
+  scrollBehavior: () => ({ y: 0 }),
+  routes,
+});
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  ...App,
+}).$mount('#app');
